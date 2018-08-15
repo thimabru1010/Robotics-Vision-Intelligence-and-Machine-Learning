a768da0081ab38227f77dc14c42c00c064de7c72
tmatheus@telecom-paristech.fr <thiago.matheus.bruno@gmail.com>
2018-08-15 15:22:18 -0300

    Lab 2 code

---
+++ asset-v1_PennX+ROBO2x+2T2017+type@asset+block@Lab2/compute_snr.m
@@ -0,0 +1,19 @@
+function snr = compute_snr(I, Id)
+
+    % Input:
+    % I: the original image
+    % Id: the approximated (noisy) image
+    % Output:
+    % snr: signal-to-noise ratio
+    
+    % Please follow the instructions in the comments to fill in the missing commands.    
+
+    % 1) Compute the noise image (original image minus the approximation)
+
+    % 2) Compute the Frobenius norm of the noise image
+    
+    % 3) Compute the Frobenius norm of the original image
+    
+    % 4) Compute SNR
+
+end
